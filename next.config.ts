e1966cecf5850f3a871bac7b3b6b2b72b89fc7ba
hr-shuvo <h.r.shuvo9@gmail.com>
2026-06-14 08:12:33 +0600

    removed unnecessary config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'api.dicebear.com' },
-    ],
-  },
 };
 
 export default nextConfig;
